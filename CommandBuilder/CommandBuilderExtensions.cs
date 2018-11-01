e7a5b5fddc31bcbdeb13f51b333af5d705dc4d87
Dominic Birch <dominicbirch@outlook.com>
2018-11-01 21:06:24 +0000

    Added multitargetting for nuget Added .AddIf extensions

--- CommandBuilder/CommandBuilderExtensions.cs
+++ CommandBuilder/CommandBuilderExtensions.cs
@@ -8,6 +8,9 @@ namespace CommandBuilder
         public static ICommandBuilder<T> Add<T>(this ICommandBuilder<T> builder, Func<T, T> action) =>
             builder.Add(new Command<T>(action));
 
+        public static ICommandBuilder<T> AddIf<T>(this ICommandBuilder<T> builder, Predicate<T> predicate, Func<T, T> action) =>
+            builder.Add(new Command<T>(i => predicate(i) ? action(i) : i));
+
         public static ICommandBuilder<T> Add<T>(this ICommandBuilder<T> builder, Action<T> action) =>
             builder.Add(new Command<T>(i =>
             {
@@ -16,13 +19,32 @@ namespace CommandBuilder
                 return i;
             }));
 
+        public static ICommandBuilder<T> AddIf<T>(this ICommandBuilder<T> builder, Predicate<T> predicate, Action<T> action) =>
+            builder.Add(new Command<T>(i =>
+            {
+                if (predicate(i)) action(i);
+
+                return i;
+            }));
+
         public static IAsyncCommandBuilder<T> Add<T>(this IAsyncCommandBuilder<T> builder, Func<T, Task<T>> action) =>
             builder.Add(new AsyncCommand<T>(action));
+
+        public static IAsyncCommandBuilder<T> AddIf<T>(this IAsyncCommandBuilder<T> builder, Predicate<T> predicate, Func<T, Task<T>> action) =>
+            builder.Add(new AsyncCommand<T>(i => predicate(i) ? action(i) : Task.FromResult(i)));
 
         public static IAsyncCommandBuilder<T> Add<T>(this IAsyncCommandBuilder<T> builder, Func<T, Task> action) =>
             builder.Add(new AsyncCommand<T>(async i =>
             {
                 await action(i);
+
+                return i;
+            }));
+
+        public static IAsyncCommandBuilder<T> AddIf<T>(this IAsyncCommandBuilder<T> builder, Predicate<T> predicate, Func<T, Task> action) =>
+            builder.Add(new AsyncCommand<T>(async i =>
+            {
+                if (predicate(i)) await action(i);
 
                 return i;
             }));
